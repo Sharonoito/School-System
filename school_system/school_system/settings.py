c991bda3abc5e1c82be48d6da6f006edb878d5b8
Sharonoito <oitosharonakoth@gmail.com>
2021-09-24 15:59:26 +0300

    added staticfiles

--- school_system/school_system/settings.py
+++ school_system/school_system/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$(h1e(!90^xo@5uni47zt9%fgc4*18_+-*dmo)k%lr0n73!r$z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sharon-schoolsystem.herokuapp.com']
 
 
 # Application definition
